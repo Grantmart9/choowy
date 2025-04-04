096f5d3066da4ba1d5b21703bcd71fe2559c22bf
Grantmart9 <grantmarthinus@gmail.com>
2025-04-04 10:19:29 +0200

    New service page

--- src/app/rent-a-service/service.js
+++ src/app/rent-a-service/service.js
@@ -87,7 +87,7 @@ const ServiceSearchBar = ({
                         size="small"
                         fullWidth={true}
                         value={searchQuery}
-                        color="error"
+                        color="info"
                         style={{
                             background: "rgba(128, 128, 128, 0.25)", // Set the background with 15% opacity
                         }}
@@ -151,9 +151,20 @@ const ServiceSearchBar = ({
     );
 };
 
+const CustomerRating = () => {
+    <Rating
+        style={{ position: "relative", top: 5, left: 5 }}
+        name="Avg rating"
+        value={Service.rating ? Service.rating : 0}
+        size="medium"
+        sx={{ alignItems: "center", justifyItems: "center" }}
+    />
+}
+
+
 const ServiceMap = ({ Data }) => {
     return (
-        <div className="grid lg:grid-cols-5 grid-cols-3 gap-4 mx-4 pb-5">
+        <div className="grid lg:grid-cols-5 grid-cols-2 gap-4 mx-4 pb-5">
             {Data.map((Service, index) => (
                 <motion.div
                     key={Service.id} // assuming each service has a unique id
@@ -173,25 +184,51 @@ const ServiceMap = ({ Data }) => {
                             alt="test"
                             style={{ maxHeight: "150pt", width: "100%" }}
                             src={`data:image/jpeg;base64,${Service.person_logo}`}
+
                         />
-                        <div className="grid grid-flow-col">
-                            <div className="flex align-center justify-center my-2">
-                                <Rating
-                                    name="Avg rating"
-                                    value={Service.rating ? Service.rating : 0}
-                                    size="medium"
-                                    sx={{ alignItems: "center", justifyItems: "center" }}
-                                />
-                            </div>
-                            <div className="flex text-gray-700 text-xs font-serif my-auto justify-center">
-                                R {Service.price} / {Service.rate_unit}
-                            </div></div>
+                        <div className="flex text-gray-700 text-xs font-serif my-auto justify-center p-2">
+                            R {Service.price} / {Service.rate_unit}
+                        </div>
                     </Stack>
                 </motion.div>
             ))}
         </div>
     );
 };
+
+//<div className="px-2.5">
+//<motion.div
+//initial={{ opacity: 0 }}
+//animate={{ opacity: 1 }}
+//transition={{
+//    delay: 2, // Add staggered delay based on index
+//    type: "keyframes",
+//    stiffness: 400,
+//    damping: 10,
+//    mass: 10,
+//    duration: 0.3,
+//}}
+//style={{
+//    background: "rgba(128, 128, 128, 0.30)", minWidth: "100%" // Set the background with 15% opacity
+//}} className="grid grid-flow-col gap-2 rounded-md p-2.5 mt-7 mx-auto" >
+//{video_url.map((video, index) =>
+//    <motion.div
+//        initial={{ opacity: 0 }}
+//        animate={{ opacity: 1 }}
+//        transition={{
+//            delay: index * 0.1, // Add staggered delay based on index
+//            type: "keyframes",
+//           stiffness: 400,
+//            damping: 10,
+//            mass: 10,
+//            duration: 0.3,
+//        }}>
+//        <video key={index} autoPlay muted loop id="video" className="mx-auto" style={{ maxHeight: "200px" }}>
+//            <source src={video.url} type="video/mp4" />
+//        </video>
+//    </motion.div>)}
+//</motion.div>
+//</div>
 
 
 const Service = () => {
@@ -275,64 +312,28 @@ const Service = () => {
 
     return (
         <React.Fragment>
-            <div>
-                <div style={{ position: "absolute", top: 0, left: 0 }}
-                    className="fixed block align-center justify-center mt-14">
+            <div className="fixed w-full mx-auto flex justify-center">
+                <ServiceSearchBar
+                    handleFilter={handleFilter}
+                    handleClose={handleClose}
+                    handleChange={handleChange}
+                    handleChange1={handleChange1}
+                    handleChange2={handleChange2}
+                    valuetext={valuetext}
+                    open={open}
+                    value={value}
+                    value1={value1}
+                    value2={value2}
+                    searchQuery={searchQuery}
+                    handleSearchChange={handleSearchChange}
+                    handleClearFilter={handleClearFilter}
+                    handleClearSearch={handleClearSearch}
+                />
+            </div>
+            <div style={{ width: "100vw" }}>
+                <div
+                    className="block align-center justify-center mt-14">
                     <div>
-                        <div className="flex align-center justify-center">
-                            <ServiceSearchBar
-                                handleFilter={handleFilter}
-                                handleClose={handleClose}
-                                handleChange={handleChange}
-                                handleChange1={handleChange1}
-                                handleChange2={handleChange2}
-                                valuetext={valuetext}
-                                open={open}
-                                value={value}
-                                value1={value1}
-                                value2={value2}
-                                searchQuery={searchQuery}
-                                handleSearchChange={handleSearchChange}
-                                handleClearFilter={handleClearFilter}
-                                handleClearSearch={handleClearSearch}
-                                className="flex align-center justify-center mb-2"
-                            />
-                        </div>
-                        <div className="px-2.5">
-                            <div>
-                                <motion.div
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: 1 }}
-                                    transition={{
-                                        delay: 2, // Add staggered delay based on index
-                                        type: "keyframes",
-                                        stiffness: 400,
-                                        damping: 10,
-                                        mass: 10,
-                                        duration: 0.3,
-                                    }}
-                                    style={{
-                                        background: "rgba(128, 128, 128, 0.30)", minWidth: "100%" // Set the background with 15% opacity
-                                    }} className="grid grid-flow-col gap-2 rounded-md p-2.5 mt-7 mx-auto" >
-                                    {video_url.map((video, index) =>
-                                        <motion.div
-                                            initial={{ opacity: 0 }}
-                                            animate={{ opacity: 1 }}
-                                            transition={{
-                                                delay: index * 0.1, // Add staggered delay based on index
-                                                type: "keyframes",
-                                                stiffness: 400,
-                                                damping: 10,
-                                                mass: 10,
-                                                duration: 0.3,
-                                            }}>
-                                            <video key={index} autoPlay muted loop id="video" className="mx-auto" style={{ maxHeight: "200px" }}>
-                                                <source src={video.url} type="video/mp4" />
-                                            </video>
-                                        </motion.div>)}
-                                </motion.div>
-                            </div>
-                        </div>
                         <div>
                             {Data.length === 0 ?
                                 <div style={{ minWidth: "100vw" }} className="flex">
