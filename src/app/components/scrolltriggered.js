401f9714d5d4617eadca9e1ccf927a967b36a302
Grant <grantmarthinus@gmail.com>
2025-04-30 13:26:04 +0200

    s

--- src/app/components/scrolltriggered.js
+++ src/app/components/scrolltriggered.js
@@ -1,115 +1,109 @@
-"use client"
+import { motion } from "framer-motion";
 import React from "react";
-import { motion } from "motion/react"
-import TopSellers from "../royalCanin.jpeg"
-import Image from "next/image";
-import { Button } from "@mui/material";
 
-export default function ScrollTriggered() {
+const ScrollTriggered = () => {
+  return (
+    <div style={container}>
+      {food.map(([emoji, hueA, hueB], i) => (
+        <Card i={i} emoji={emoji} hueA={hueA} hueB={hueB} key={emoji} />
+      ))}
+    </div>
+  );
+};
 
-    return (
-        <div style={container}>
-            {food.map(([emoji, hueA, hueB], i) => (
-                <Card i={i} emoji={emoji} hueA={hueA} hueB={hueB} key={emoji} />
-            ))}
-        </div>
-    );
-}
+const Card = ({ emoji, hueA, hueB, i }) => {
+  const background = `linear-gradient(306deg, ${hue(hueA)}, ${hue(hueB)})`;
 
-function Card({ emoji, hueA, hueB, i }) {
-    const background = `linear-gradient(306deg, ${hue(hueA)}, ${hue(hueB)})`;
-
-    return (
-
-        <motion.div
-            className={`card-container-${i}`}
-            style={cardContainer}
-            initial="offscreen"
-            whileInView="onscreen"
-            viewport={{ amount: 1 }}
-        >
-            <div style={{ ...splash, background, width: "100%" }} />
-            <Button disableRipple={true}>
-                <motion.div className="bg-[url(./background2.svg)] bg-repeat bg-cover p-1" style={card} variants={cardVariants} className="card">
-                    {emoji}
-                </motion.div>
-            </Button>
-        </motion.div>
-
-
-    );
-}
+  return (
+    <motion.div
+      className={`card-container-${i}`}
+      style={cardContainer}
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ amount: 0.8 }}
+    >
+      <div style={{ ...splash, background }} />
+      <motion.div style={card} variants={cardVariants} className="card">
+        {emoji}
+      </motion.div>
+    </motion.div>
+  );
+};
 
 const cardVariants = {
-    offscreen: {
-        y: 300,
+  offscreen: {
+    y: 300,
+  },
+  onscreen: {
+    y: 50,
+    rotate: -10,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8,
     },
-    onscreen: {
-        y: 50,
-        rotate: -10,
-        transition: {
-            type: "spring",
-            bounce: 0.4,
-            duration: 0.8,
-        },
-    },
+  },
 };
 
 const hue = (h) => `hsl(${h}, 100%, 50%)`;
 
 /**
-* ==============   Styles   ================
-*/
+ * ==============   Styles   ================
+ */
 
 const container = {
-    margin: "0px auto",
-    maxWidth: 500,
-    paddingBottom: 100,
-    width: "100%",
+  margin: "100px auto",
+  maxWidth: 500,
+  paddingBottom: 100,
+  width: "100%",
 };
 
 const cardContainer = {
-    overflow: "hidden",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "relative",
-    paddingTop: 20,
-    paddingLeft: 5,
-    paddingRight: 5,
-    marginBottom: -120,
+  overflow: "hidden",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "relative",
+  paddingTop: 20,
+  marginBottom: -120,
 };
 
 const splash = {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    clipPath: `path("M 14 303.5 C 14 292.454 21.8 285.101 29 283.5 L 414 219.5 C 424 218.033 434 228.454 434 239.5 L 428 430 C 428 441.046 420 450 410 450 L 15 450 C 7.8 450 0 441.046 0 430 Z")`,
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  clipPath: `path("M 0 303.5 C 0 292.454 8.995 285.101 20 283.5 L 460 219.5 C 470.085 218.033 480 228.454 480 239.5 L 500 430 C 500 441.046 491.046 450 480 450 L 20 450 C 8.954 450 0 441.046 0 430 Z")`,
 };
 
 const card = {
-    fontSize: 164,
-    width: 300,
-    height: 430,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 20,
-    background: "#f5f5f5",
-    boxShadow:
-        "0 0 1px hsl(0deg 0% 0% / 0.075), 0 0 2px hsl(0deg 0% 0% / 0.075), 0 0 4px hsl(0deg 0% 0% / 0.075), 0 0 8px hsl(0deg 0% 0% / 0.075), 0 0 16px hsl(0deg 0% 0% / 0.075)",
-    transformOrigin: "10% 60%",
+  fontSize: 164,
+  width: 300,
+  height: 430,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  borderRadius: 20,
+  background: "#f5f5f5",
+  boxShadow:
+    "0 0 1px hsl(0deg 0% 0% / 0.075), 0 0 2px hsl(0deg 0% 0% / 0.075), 0 0 4px hsl(0deg 0% 0% / 0.075), 0 0 8px hsl(0deg 0% 0% / 0.075), 0 0 16px hsl(0deg 0% 0% / 0.075)",
+  transformOrigin: "10% 60%",
 };
 
+/**
+ * ==============   Data   ================
+ */
+
 const food = [
-    [<Image src={TopSellers} alt={"product"} />, 340, 10],
-    [<Image src={TopSellers} alt={"product"} />, 20, 40],
-    [<Image src={TopSellers} alt={"product"} />, 60, 90],
-    [<Image src={TopSellers} alt={"product"} />, 80, 120],
-    [<Image src={TopSellers} alt={"product"} />, 100, 140],
-    [<Image src={TopSellers} alt={"product"} />, 205, 245],
-    [<Image src={TopSellers} alt={"product"} />, 260, 290],
-    [<Image src={TopSellers} alt={"product"} />, 290, 320],
+  ["🍅", 340, 10],
+  ["🍊", 20, 40],
+  ["🍋", 60, 90],
+  ["🍐", 80, 120],
+  ["🍏", 100, 140],
+  ["🫐", 205, 245],
+  ["🍆", 260, 290],
+  ["🍇", 290, 320],
 ];
+
+export default ScrollTriggered;
--- src/app/page.js
+++ src/app/page.js
@@ -5,61 +5,8 @@ import ScrollTriggered from "./components/scrolltriggered";
 import { FontType, SUPABASE_URL_CLOUDCRAFT, API_KEY_CLOUDCRAFT } from "./supabase";
 import { createClient } from "@supabase/supabase-js";
 import LoadingThreeDotsJumping from "./components/loading";
-import ScrollLinked2 from "./components/scrollLinked2";
-import { motion } from "motion/react";
-
 
 const supabase = createClient(SUPABASE_URL_CLOUDCRAFT, API_KEY_CLOUDCRAFT);
-
-const CircleOfDots = () => {
-  const dotCount = 5; // Number of dots
-  const radius = 200; // Radius of the circle in pixels
-  const centerX = 0; // X-coordinate of the circle center
-  const centerY = 0; // Y-coordinate of the circle center
-
-  // Generate dot positions based on circle geometry
-  const dots = Array.from({ length: dotCount }).map((_, index) => {
-    const angle = (index / dotCount) * 2 * Math.PI; // Angle in radians
-    const x = centerX + radius * Math.cos(angle); // X-position
-    const y = centerY + radius * Math.sin(angle); // Y-position
-    return { x, y };
-  });
-
-  return (
-    <div
-      className="mx-auto"
-      style={{
-        width: radius * 2 + 50, // Circle container size
-        height: radius * 2 + 50,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {dots.map((dot, i) => (
-        <motion.div
-          key={i}
-          initial={{ backgroundColor: "#038067", scale: 1 }}
-          animate={{ backgroundColor: "#05edbf", scale: 1 }}
-          transition={{
-            duration: 5,
-            repeat: Infinity,
-            delay: i * 0.1,
-            ease: "circInOut",
-          }}
-          style={{
-            position: "absolute",
-            width: 100,
-            height: 100,
-            borderRadius: "50%",
-            top: `${dot.y + radius}px`,
-            left: `${dot.x + radius}px`,
-          }}
-        />
-      ))}
-    </div>
-  );
-};
 
 export default function Home() {
   const [Data, setData] = useState([]);
@@ -78,22 +25,30 @@ export default function Home() {
   return (
     <React.Fragment>
       <div className="mt-12 block align-center justify-center" >
+        <div style={{ fontFamily: FontType, color: "whitesmoke" }} className="text-left  ml-2">Top Sellers</div>
         {Data.length === 0 ?
           <div style={{ minWidth: "100vw", marginTop: "40vh" }} className="flex">
             <div
               className="mx-auto">
               <LoadingThreeDotsJumping />
             </div>
-          </div> :
-          <div>
-            <text className={`text-cyan-100 font-bold ${FontType} text-xl`}>Top Sellers</text>
-            <ScrollLinked2 />
-            <text className={`text-cyan-100 font-bold ${FontType} text-xl mt-3`}>Top Rated</text>
-            <ScrollLinked2 />
-            <text className={`text-cyan-100 font-bold ${FontType} text-xl mt-3`}>Recently Added</text>
-            <ScrollLinked2 />
-          </div>
-        }
+          </div> : <ScrollTriggered Data={Data} />}
+        <div style={{ fontFamily: FontType, color: "whitesmoke" }} className="text-left  ml-2">Top Rated</div>
+        {Data.length === 0 ?
+          <div style={{ minWidth: "100vw", marginTop: "40vh" }} className="flex">
+            <div
+              className="mx-auto">
+              <LoadingThreeDotsJumping />
+            </div>
+          </div> : <ScrollTriggered Data={Data} />}
+        <div style={{ fontFamily: FontType, color: "whitesmoke" }} className="text-left  ml-2">New Arivals</div>
+        {Data.length === 0 ?
+          <div style={{ minWidth: "100vw", marginTop: "40vh" }} className="flex">
+            <div
+              className="mx-auto">
+              <LoadingThreeDotsJumping />
+            </div>
+          </div> : <ScrollTriggered Data={Data} />}
       </div>
     </React.Fragment>
   );
